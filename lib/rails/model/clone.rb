1ec7c262721472be0b394daf1225f09f3d36ef76
lwoodson <lance@webmaneuvers.com>
2014-03-01 06:50:58 -0600

    Gemification

---
+++ lib/rails/model/clone.rb
@@ -0,0 +1,9 @@
+require "rails/model/clone/version"
+
+module Rails
+  module Model
+    module Clone
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rails/model/clone/version.rb
@@ -0,0 +1,7 @@
+module Rails
+  module Model
+    module Clone
+      VERSION = "0.0.1"
+    end
+  end
+end
